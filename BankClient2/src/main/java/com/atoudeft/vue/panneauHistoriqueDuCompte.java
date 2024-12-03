30fd446ff46c94bdf2df11a44a1883518bda858d
mathisada <mathis-ada@hotmail.com>
2024-12-03 10:48:46 -0500

    Correction

--- BankClient2/src/main/java/com/atoudeft/vue/panneauHistoriqueDuCompte.java
+++ BankClient2/src/main/java/com/atoudeft/vue/panneauHistoriqueDuCompte.java
@@ -5,10 +5,10 @@ import java.awt.*;
 
 public class panneauHistoriqueDuCompte extends JPanel {
     public panneauHistoriqueDuCompte(String historique) {
-            JTextArea histogramme = new JTextArea(historique);
-            histogramme.setEditable(false);
+        JTextArea histogramme = new JTextArea(historique);
+        histogramme.setEditable(false);
 
-            JScrollPane defilement = new JScrollPane(histogramme);
-            add(defilement, BorderLayout.CENTER);
-        }
+        JScrollPane defilement = new JScrollPane(histogramme);
+        add(defilement, BorderLayout.CENTER);
     }
+}
