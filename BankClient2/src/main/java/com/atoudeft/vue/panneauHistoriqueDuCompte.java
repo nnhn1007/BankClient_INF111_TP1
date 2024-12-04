bd0e4299e68df25d1494f3127d055111ea3b6ef1
nancynhn <156736092+nancynhn@users.noreply.github.com>
2024-12-04 01:26:10 -0500

    JavaDoc - panneauHistoriqueDuCompte

--- BankClient2/src/main/java/com/atoudeft/vue/panneauHistoriqueDuCompte.java
+++ BankClient2/src/main/java/com/atoudeft/vue/panneauHistoriqueDuCompte.java
@@ -3,7 +3,17 @@ package com.atoudeft.vue;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ *  La classe panneauHistoriqueDuCompte est un panneau permettant d'afficher l'historique des opérations d'un compte
+ *
+ * @author Mathis Odjo'o Ada
+ */
 public class panneauHistoriqueDuCompte extends JPanel {
+
+    /**
+     * Constructeur de la classe panneauHistoriqueDuCompte
+     * @param historique chaîne caractères affaichant l'historique des opérations d'un compte
+     */
     public panneauHistoriqueDuCompte(String historique) {
         JTextArea histogramme = new JTextArea(historique);
         histogramme.setEditable(false);
